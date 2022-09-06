f3c67c9deb7aada45851510e27e6be375935912c
Kronomant <milardlucas@gmail.com>
2022-09-05 23:08:20 -0300

    feature: home cover

--- src/Containers/Home/Home.tsx
+++ src/Containers/Home/Home.tsx
@@ -1,38 +1,42 @@
-import { Container, Flex, Grid, HStack, Text } from '@chakra-ui/react'
+import {  Flex, Grid,  Text, Image } from '@chakra-ui/react'
+import Header from 'Components/Core/Header'
 import React from 'react'
 
 import { FiGithub } from 'react-icons/fi'
 
 const Home: React.FC = () => (
-	<Container size="container.xl">
-		<HStack justifyContent="space-between" p={2}>
-			<Text fontFamily="Poppins">Star</Text>
-			<HStack spacing={20} justifyContent="space-between">
-				<Text fontFamily="Poppins">Home</Text>
-				<Text fontFamily="Poppins">About</Text>
-				<Text fontFamily="Poppins">Start</Text>
-				
-			</HStack>
-		</HStack>
+	<Flex flexDir="column"  width="100%">
+		<Header />
 		<Flex>
-			<Grid w="20%">
-				<FiGithub color="blue" />
+			<Grid  justifyContent="center" w="10%">
+			<FiGithub color="blue" />
 			</Grid>
-			<Flex w="80%">
-				<Grid w="50%">
-					<Text>Classificando estrelas e Galaxias</Text>
-					<Text> Trabalho inter disciplinar VI </Text>
-					<Text>
+			<Flex gap={8} w="90%">
+				<Grid w="25%">
+					<Flex flexDir={"column"}>
+						<Text 
+						 fontFamily="Poppins" 
+						 fontSize="3xl" 
+						 fontWeight="semibold" 
+						 color="purple.500">
+						Classificando estrelas e Galaxias
+						</Text>
+						<Text fontFamily="Poppins"  fontSize="xl" > Trabalho inter disciplinar VI </Text>
+					</Flex>
+					
+					<Text fontFamily="Poppins">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
 						labore, fugiat maiores doloribus enim dicta. Labore animi nesciunt
 						sequi iste reiciendis quisquam voluptate laudantium consequuntur, ut
 						laborum magni beatae ab.
 					</Text>
 				</Grid>
-				<Flex w="50%"></Flex>
+				<Flex w="70%">
+					<Image src='/images/cover.jpg' />
+				</Flex>
 			</Flex>
 		</Flex>
-	</Container>
+	</Flex>
 )
 
 export default Home
