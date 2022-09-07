706a9aad0fc59d00748dae8cdc343d12e4f1847a
Kronomant <milardlucas@gmail.com>
2022-09-07 10:15:53 -0300

    feat: eslint

--- src/Components/Core/Header/Header.tsx
+++ src/Components/Core/Header/Header.tsx
@@ -1,19 +1,18 @@
-
 import React from 'react'
 
 import { HStack, Text } from '@chakra-ui/react'
 
-
 const Header = () => (
-    <HStack justifyContent="space-between" p={8}>
-			<Text fontSize={"2xl"} fontFamily="Poppins">Star</Text>
-			<HStack spacing={20} justifyContent="space-between">
-				<Text  fontFamily="Poppins">Home</Text>
-				<Text fontFamily="Poppins">About</Text>
-				<Text fontFamily="Poppins">Start</Text>
-			</HStack>
+	<HStack justifyContent="space-between" p={8}>
+		<Text fontSize="2xl" fontFamily="Poppins">
+			Star
+		</Text>
+		<HStack spacing={20} justifyContent="space-between">
+			<Text fontFamily="Poppins">Home</Text>
+			<Text fontFamily="Poppins">About</Text>
+			<Text fontFamily="Poppins">Start</Text>
+		</HStack>
 	</HStack>
 )
 
-
-export default Header 
+export default Header
--- src/Containers/Home/Home.tsx
+++ src/Containers/Home/Home.tsx
@@ -1,29 +1,32 @@
-import {  Flex, Grid,  Text, Image } from '@chakra-ui/react'
+import { Flex, Grid, Text, Image } from '@chakra-ui/react'
 import Header from 'Components/Core/Header'
 import React from 'react'
 
 import { FiGithub } from 'react-icons/fi'
 
 const Home: React.FC = () => (
-	<Flex flexDir="column"  width="100%">
+	<Flex flexDir="column" width="100%">
 		<Header />
 		<Flex>
-			<Grid  justifyContent="center" w="10%">
-			<FiGithub color="blue" />
+			<Grid justifyContent="center" w="10%">
+				<FiGithub color="blue" />
 			</Grid>
 			<Flex gap={8} w="90%">
 				<Grid w="25%">
-					<Flex flexDir={"column"}>
-						<Text 
-						 fontFamily="Poppins" 
-						 fontSize="3xl" 
-						 fontWeight="semibold" 
-						 color="purple.500">
-						Classificando estrelas e Galaxias
+					<Flex flexDir="column">
+						<Text
+							fontFamily="Poppins"
+							fontSize="3xl"
+							fontWeight="semibold"
+							color="purple.500"
+						>
+							Classificando estrelas e Galaxias
 						</Text>
-						<Text fontFamily="Poppins"  fontSize="xl" > Trabalho inter disciplinar VI </Text>
+						<Text fontFamily="Poppins" fontSize="xl">
+							Trabalho inter disciplinar VI
+						</Text>
 					</Flex>
-					
+
 					<Text fontFamily="Poppins">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
 						labore, fugiat maiores doloribus enim dicta. Labore animi nesciunt
@@ -32,7 +35,7 @@ const Home: React.FC = () => (
 					</Text>
 				</Grid>
 				<Flex w="70%">
-					<Image src='/images/cover.jpg' />
+					<Image src="/images/cover.jpg" />
 				</Flex>
 			</Flex>
 		</Flex>
