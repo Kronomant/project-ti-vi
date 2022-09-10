a49f67fd49be51fc547942bf109723a25135b75c
Kronomant <milardlucas@gmail.com>
2022-09-10 11:25:46 -0300

    feat: initializing classificator page

---
+++ src/Containers/Classificator/Classificator.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Flex } from '@chakra-ui/react'
+import Header from 'Components/Core/Header'
+
+const Classificator = () => (
+	<Flex flexDir="column">
+		<Header />
+	</Flex>
+)
+
+export default Classificator
