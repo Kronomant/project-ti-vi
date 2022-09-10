08be6a8e34f3b9a2112cc423e3485824abf5a282
Kronomant <milardlucas@gmail.com>
2022-09-10 17:47:31 -0300

    feature: classificator page

--- src/Components/Core/Header/Header.tsx
+++ src/Components/Core/Header/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
-import { HStack, Text } from '@chakra-ui/react'
+import { HStack, Text, Image, Flex } from '@chakra-ui/react'
 
 const Header = () => (
-	<HStack justifyContent="space-between" p={8}>
-		<Text fontSize="2xl" fontFamily="Poppins">
-			Star
-		</Text>
+	<HStack p="20px 32px" justifyContent="space-between">
+		<Flex alignItems="center" gap="12px">
+			<Image w="24px" height="24px" src="/favorite.png" />
+			<Text fontSize="2xl" fontFamily="Poppins" fontWeight="semibold">
+				TI VI
+			</Text>
+		</Flex>
+
 		<HStack spacing={20} justifyContent="space-between">
 			<Text fontFamily="Poppins">Home</Text>
 			<Text fontFamily="Poppins">About</Text>
---
+++ src/Components/Presentation/InputImage/InputImage.style.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const ImageContainer = styled.div`
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	cursor: pointer;
+
+	svg {
+		position: absolute;
+		left: calc(50% - 10px);
+		top: calc(50% - 10px);
+		transform: scale(1.5);
+		cursor: pointer;
+	}
+`
+export const ImageCover = styled.div`
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 400px;
+	width: 800px;
+	margin: 16px 0;
+	border: 2px dashed #d53f8c;
+
+	img {
+		object-fit: cover;
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+	}
+`
