ec10e4cce6aacc161c760fa5c7b10969c1c99bbd
Kronomant <milardlucas@gmail.com>
2022-11-04 10:23:43 -0300

    deploy heroku

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const API = axios.create({
-	//baseURL: 'https://class-estrelas-galaxias.herokuapp.com/',
-	baseURL: 'http://127.0.0.1:5000',
+	baseURL: 'https://class-estrelas-galaxias.herokuapp.com/',
+	//baseURL: 'http://127.0.0.1:5000',
 	headers: {
 		AccessControlAllowOrigin: true
 	}
